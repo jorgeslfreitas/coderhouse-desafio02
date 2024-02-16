adec732be98851d2e85005ad511d1c9b4310bedb
jorgeslfreitas <jorgeslfreitas@gmail.com>
2024-02-15 22:53:10 -0300

    melhorias do codigo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 
 let peso = Number(prompt("Qual seu peso:"));
 let altura = Number(prompt("Qual sua altura:"));
-let classificacao = document.querySelector("#classificacao");
+let paragrafo = document.querySelector("#paragrafo");
+let imgHtml = document.querySelector("img");
 
 function calculoIMC(peso, altura) {
     let imc = peso / (altura * altura);
@@ -10,22 +11,42 @@ function calculoIMC(peso, altura) {
 }
 
 let imc = calculoIMC(peso, altura).toFixed(2);
-console.log(imc);
 
-function classificacaoIMC(imc, classificacao) {
+function IMC(imc) {
     if (imc <= 18.5) {
-        classificacao.innerHTML = `IMC = ${imc} - Abaixo do peso`;
+        return {
+            text: `IMC = ${imc} - Abaixo do peso`, 
+            img : `img/abaixo-peso.png`,
+        }
+            
     } else if (imc > 18.6 && imc <= 24.9) {
-        classificacao.innerHTML = `IMC = ${imc} - Peso ideal`;
+        return {
+            text: `IMC = ${imc} - Peso ideal`,
+            img: `img/peso-normal.png`,
+        }
     } else if (imc > 25 && imc <= 29.9) {
-        classificacao.innerHTML = `IMC = ${imc} - Levemente acima do peso`;
+        return {
+            text: `IMC = ${imc} - Levemente acima do peso`,
+            img: `img/acima-peso.png`,
+        }
     } else if (imc > 30 && imc <= 34.9) {
-        classificacao.innerHTML = `IMC = ${imc} - Obesidade grau 1`;
-    } else if (imc > 35 && imc <= 39.9) {
-        classificacao.innerHTML = `IMC = ${imc} - Obesidade grau 2`;
+        return {
+            text: `IMC = ${imc} - Obesidade grau 1`,
+            img: `img/obesidade-1.png`,
+        }
     } else {
-        classificacao.innerHTML = `IMC = ${imc} - Obesidade grau 3`;
+        return {
+            text: `IMC = ${imc} - Obesidade grau 2`,
+            img: `img/obesidade-2.png`
+        }
     }
 }
 
-classificacaoIMC(imc, classificacao);
+let classificacaoIMC = IMC(imc);
+
+function resultadoIMC(classificacaoIMC, paragrafo, img) {
+    paragrafo.innerHTML = classificacaoIMC.text;
+    img.src = classificacaoIMC.img;
+}
+
+resultadoIMC(classificacaoIMC, paragrafo, img);
